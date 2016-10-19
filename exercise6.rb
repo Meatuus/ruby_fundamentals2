fd3e0f2137904cdb2d2fd7bc43beb6921a5593d0
Tyler <tcameronsmith@gmail.com>
2016-10-19 15:49:59 -0400

    update shopping list with a method

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,21 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+grocery_list << "rice"
+
+def list(a)
+	a.each do |x|
+		puts "* " + x
+	end
+end
+
+list(grocery_list)
+
+puts grocery_list.length
+
+if grocery_list.include?("bananas")
+	puts "You need to pick up bananas"
+else
+	puts "You don't need any bananas"
+end
+
+puts grocery_list[1]
